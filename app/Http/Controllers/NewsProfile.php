133e7f5e5568a3addc15001232a7394bebab4b1f
eliscioter~ <attila433lehun@gmail.com>
2022-07-10 20:07:16 +0800

    test

--- app/Http/Controllers/NewsProfile.php
+++ app/Http/Controllers/NewsProfile.php
@@ -8,22 +8,22 @@ use Illuminate\Support\Facades\Http;
 class NewsProfile extends Controller
 {
     public function headlineList() {
-        /* $url = 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=618518684c3a4b948ce36d93ea6e72c9';
-        $data = Http::get($url)->json(); */
+        $url = 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=618518684c3a4b948ce36d93ea6e72c9';
+        $data = Http::get($url)->json();
         // $response = $data->json();
 
         
 
-        // return view('news_headline', ['data' => $data]);
-        return view('news_headline');
+        return view('news_headline', ['data' => $data]);
+        // return view('news_headline');
         // return Http::get('https://newsapi.org/v2/top-headlines?country=us&apiKey=618518684c3a4b948ce36d93ea6e72c9')->body();
     }
 
     public function techList() {
-       /*  $url = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=618518684c3a4b948ce36d93ea6e72c9';
+        $url = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=618518684c3a4b948ce36d93ea6e72c9';
         $data = Http::get($url)->json();
 
-        return view('news_tech', ['data' => $data]); */
-        return view('news_tech');
+        return view('news_tech', ['data' => $data]);
+        // return view('news_tech');
     }
 }
