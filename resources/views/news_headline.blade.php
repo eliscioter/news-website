cfa55501b628e35b74ca754cd4e3a2f9971fe463
eliscioter~ <attila433lehun@gmail.com>
2022-07-11 16:27:07 +0800

    utilized html semantics

--- resources/views/news_headline.blade.php
+++ resources/views/news_headline.blade.php
@@ -3,61 +3,59 @@
 
 @section('css')
     <style>
-        body {
-                font-family: 'Nunito', sans-serif;
-                background-color: #010b13;
-                color: #fff;
+        .news-section {
+            height: 100%;
+            display: grid;
+            place-items: center;
+            padding: 20px;
+        }
+        .card {
+            border: 1px solid #9a9a9a;
+            border-radius: 10px;
+            width: 50%;
+            margin-bottom: 20px;
+            padding: 10px;
+            box-shadow: 0 4px 2px -2px #778899;
+        }
+        **.card-img {
+            max-width: 25%;
+            margin: 0;
+        }
+        .news-img {
+            max-width: 100%;
+            padding: 5px;
+        }
+        **.card-body {
+            max-width: 100%;
+        }
+
+        @media (min-width: 768px) {
+            **.card-img {
+                display: inline-block;
+                margin: 0;
             }
-            .news-section {
-                height: 100%;
-                display: grid;
-                place-items: center;
-                padding: 20px;
-            }
+        }
+        @media (min-width: 768px) and (max-width: 992px) {
             .card {
-                border: 1px solid #9a9a9a;
-                border-radius: 10px;
-                width: 50%;
-                margin-bottom: 20px;
-                padding: 10px;
-                
+                width:75%;
+            }
+        }
+        @media (min-width: 576px) and (max-width: 768px) {
+            .card-img {
+                width: 100%;
+            }
+        }
+        @media (max-width: 576px) {
+            .card {
+                width: 100%;
             }
             .card-img {
-                max-width: 25%;
-            }
-            .news-img {
-                max-height: 100%;
                 max-width: 100%;
-                padding: 5px;
             }
             
+        }
 
-            @media (min-width: 768px) {
-                .card-content {
-                    display: flex;
-                }
-            }
-            @media (min-width: 768px) and (max-width: 992px) {
-                .card {
-                    width:75%;
-                }
-            }
-            @media (min-width: 576px) and (max-width: 768px) {
-                .card-img {
-                    width: 100%;
-                }
-            }
-            @media (max-width: 576px) {
-                .card {
-                    width: 100%;
-                }
-                .card-img {
-                    max-width: 100%;
-                }
-                
-            }
-
-        </style>
+    </style>
 @endsection
 
 
@@ -71,26 +69,23 @@
                             {{$data['articles'][$i]['title']}}
                         </h1> 
                         <br/>
-                        <span style="font-size: 0.5rem;">
+                        <time style="font-size: 0.5rem;">
                             {{$data['articles'][$i]['publishedAt']}}
-                        </span>
+                        </time>
                     </div>
                     <div class="card-content">
-                        <div class="card-img">
+                        <figure class="card-img">
                             <img src={{$data['articles'][$i]['urlToImage']}} alt={{$data['articles'][$i]['description']}} class="news-img">
-                        </div>
-                        <div class="card-body">
-                            <p>
-                                {{$data['articles'][$i]['description']}}
-                            </p>
-                            <a href={{$data['articles'][$i]['url']}} target="_blank" style="font-size: 0.7rem; float:right; color: #d0d0d0;">Read more &raquo;</a>
-                            <br/>
-                        
-                        </div>
+                            <figcaption class="card-body">
+                                    {{$data['articles'][$i]['description']}}
+                            </figcaption>
+                        </figure>      
+                        <a href={{$data['articles'][$i]['url']}} target="_blank" style="font-size: 0.7rem; float:right; color: #d0d0d0; padding-right: 10px">Read more &raquo;</a>
                     </div>
-                    <span style="font-size: 0.5rem;">
+                    <br/>
+                    <i style="font-size: 0.5rem;"> 
                         - {{$data['articles'][$i]['source']['name']}}
-                    </span>
+                    </i>
                 </div>
             @endfor
         </section>
